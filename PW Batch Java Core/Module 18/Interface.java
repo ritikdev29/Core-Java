997ed2358dac8be0d48c1bc04f0ac99a20c01700
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-06-14 14:50:58 +0530

    What is interface

---
+++ PW Batch Java Core/Module 18/Interface.java
@@ -0,0 +1,15 @@
+import java.util.*;
+interface A{
+    int num = 7;
+    void show();
+
+    
+}
+public class Interface {
+    public static void main(String[] args) {
+        System.out.println(A.num);
+        // A obj = new obj(); this is not creted object or not support....
+        
+    }
+    
+}
