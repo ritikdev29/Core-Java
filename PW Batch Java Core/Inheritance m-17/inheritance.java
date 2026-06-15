40c4b08606251c59cbe6a95799620626c4f09774
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-06-15 12:48:46 +0530

    .

--- PW Batch Java Core/Inheritance m-17/inheritance.java
+++ PW Batch Java Core/Inheritance m-17/inheritance.java
@@ -1,9 +1,39 @@
 import java.util.*;
-class Human {
-    
+class Human1
+{
+ private String name;
+ int age;
+ Human1()
+ {
+    System.out.println("Human class constructor....");
+ }
+ void sleep()
+ {
+    age = 18;
+    name = "Ritik";
+    System.out.println("Human needs Good sleep..");
+    System.out.println(age);
+    System.out.println(name);
+ }   
+}
+class Student1 extends Human1
+{
+    // // public Student1()
+    // //{ 
+    // super();
+
+    // }
+    void disp()
+    {
+        System.out.println("The age is : " + age);
+        // System.out.println("The name is : " + name);
+    }
 }
 public class inheritance{
     public static void main (String []args){
+        Student1 str = new Student1();
+        str.sleep();
+        str.disp();
 
     }
 }
