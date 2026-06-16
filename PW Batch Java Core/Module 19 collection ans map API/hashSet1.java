b5cd95d45cc5046b8dcd1f2f48eaf449b69c21e4
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-06-16 17:44:56 +0530

    hashSet

---
+++ PW Batch Java Core/Module 19 collection ans map API/hashSet1.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class hashSet1 {
+    public static void main(String[] args) {
+
+        HashSet<String> names = new HashSet<>();
+
+        names.add("Ritik");
+        names.add("Aman");
+        names.add("Ritik");
+
+        System.out.println(names);
+
+        // output = [Aman, Ritik]
+        /*
+         * mportant Methods
+         * add()
+         * remove()
+         * contains()
+         * size()
+         * clear()
+         */
+    }
+}
