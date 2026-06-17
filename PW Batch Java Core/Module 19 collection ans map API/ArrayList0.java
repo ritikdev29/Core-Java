b72b5962c2445309f303851142a6cb7afddc18fe
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-06-17 12:23:21 +0530

    Array List concept

---
+++ PW Batch Java Core/Module 19 collection ans map API/ArrayList0.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class ArrayList0 {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Ritik");
+        names.add("Aman");
+        names.add("Ritik");
+
+        System.out.println(names);
+
+        // output = [Ritik, Aman, Ritik]
+
+        /*
+         * Importance method
+         * add()
+         * get()
+         * set()
+         * remove()
+         * contains()
+         * size()
+         * isEmpty()
+         * clear()
+         */
+    }
+}
