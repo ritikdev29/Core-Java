813e332263a7c320565366f7dbd22a09db138908
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-06-18 17:04:48 +0530

    Need of Generics and Basics of Generics

---
+++ PW Batch Java Core/Module 20 Generic And File Handling/generics_01.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+public class generics_01 {
+    public static void main(String[] args) {
+
+        // typeSafety
+       /*  String ar [] = new String[10];
+        ar[0] ="Rider";
+        ar[1] = "Hider";
+        ar[2] = "sk";
+        ar[3] = 20; //CE 
+        */
+
+  // generics mai bta dete hai kis data par work arna hai jaise ismai String btaya gya hai
+        // No safety ---->
+
+        // Typesafety achived using generics ---->
+        ArrayList<String> tr = new ArrayList<String>();  // <> this is used for typecasting like String <String>
+        tr.add("Rk");
+        tr.add("GK");
+
+        //tr.add(20);
+
+        //String n1 = (String)tr.get(0); // typecasting
+       // String n2 = (String)tr.get(1);
+        //String n3 = (String)tr.get(2);
+
+        
+    }
+    
+}
